939003b0c78ab2fac725861f42a5af79cc6de175
Pranav Addepalli <pranav.addepalli@gmail.com>
2022-11-05 19:59:43 -0400

    Added firestore to project, demo for pulling data

---
+++ CMUMeets/CMUMeets/Views/HostView.swift
@@ -0,0 +1,37 @@
+//
+//  HostView.swift
+//  CMUMeets
+//
+//  Created by Pranav Addepalli on 11/5/22.
+//
+
+import SwiftUI
+import FirebaseFirestore
+
+func test() -> String {
+    var db = Firestore.firestore()
+    db.collection("users").getDocuments() { (querySnapshot, err) in
+        if let err = err {
+            print("Error getting documents: \(err)")
+        } else {
+            for document in querySnapshot!.documents {
+                print("\(document.documentID) => \(document.data())")
+            }
+        }
+    }
+    
+    return "success"
+}
+
+struct HostView: View {
+    var body: some View {
+        Text("host meet page")
+        Text(test())
+    }
+}
+
+struct HostView_Previews: PreviewProvider {
+    static var previews: some View {
+        HostView()
+    }
+}
