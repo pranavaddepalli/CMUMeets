cfcac29b8dadd15052daaa012eaa049d7fd5f331
Pranav Addepalli <pranav.addepalli@gmail.com>
2022-11-05 18:27:56 -0400

    Created project in xcode

---
+++ CMUMeets/CMUMeets/CMUMeetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CMUMeetsApp.swift
+//  CMUMeets
+//
+//  Created by Pranav Addepalli on 11/5/22.
+//
+
+import SwiftUI
+
+@main
+struct CMUMeetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
