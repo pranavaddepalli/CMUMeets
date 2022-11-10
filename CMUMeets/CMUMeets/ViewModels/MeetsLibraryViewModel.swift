f6b3f7947c2e6078b9a964c9844891e949127b42
Isaac Ahn <75589282+isaacahn01@users.noreply.github.com>
2022-11-10 02:43:47 -0500

    ViewModels Made

---
+++ CMUMeets/CMUMeets/ViewModels/MeetsLibraryViewModel.swift
@@ -0,0 +1,41 @@
+//
+//  MeetsLibraryViewModel.swift
+//  Sprint4
+//
+//  Created by Isaac Ahn on 11/7/22.
+//
+
+import Foundation
+import Combine
+
+class MeetsLibraryViewModel: ObservableObject {
+  @Published var meetViewModels: [MeetViewModel] = []
+  private var cancellables: Set<AnyCancellable> = []
+
+  @Published var meetsRepository = MeetsRepository()
+  @Published var library: [Meet] = []
+    
+  @Published var usersRepository = UsersRepository()
+  @Published var userLibrary: [User] = []
+    
+  init() {
+    meetsRepository.$meets.map { meets in
+      meets.map(MeetViewModel.init)
+    }
+    .assign(to: \.meetViewModels, on: self)
+    .store(in: &cancellables)
+      
+    getUsers()
+  }
+
+  func add(_ meet: Meet) {
+    meetsRepository.add(meet)
+  }
+    
+  func getUsers() {
+    userLibrary = usersRepository.users
+    //print(usersRepository.printUsers())
+      print(meetsRepository.meets)
+  }
+    
+}
