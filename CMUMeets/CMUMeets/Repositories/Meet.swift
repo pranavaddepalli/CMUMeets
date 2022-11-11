329fab7ad46dc06b50d7e78595c719749e0b1f68
Isaac Ahn <75589282+isaacahn01@users.noreply.github.com>
2022-11-11 03:33:30 -0500

    Updated Meets Model

--- CMUMeets/CMUMeets/Repositories/Meet.swift
+++ CMUMeets/CMUMeets/Repositories/Meet.swift
@@ -7,30 +7,51 @@
 
 import Foundation
 import SwiftUI
+import FirebaseFirestore
 import FirebaseFirestoreSwift
+
 struct Meet: Codable, Identifiable , Comparable {
-  @DocumentID var id: String?
-  var title: String
-  var location: String
-  var timeStart: TimeInterval
-  var timeEnd: Date
-  var host: User
-  var people: [User]
-  var capacity: Int
-//  enum CodingKeys: String, CodingKey {
-//    case id
-//    case location
-//    case timeStart
-//    case timeEnd
-//    case host
-//    case people
-//    case capacity
-//  }
-//
-  static func ==(lhs: Meet, rhs: Meet) -> Bool {
-    return lhs.title == rhs.title && lhs.host == rhs.host
-  }
-  static func <(lhs: Meet, rhs: Meet) -> Bool {
-   return lhs.title < rhs.title
-  }
+    @DocumentID var id: String?
+    var title: String
+    var location: String
+    var timeStart: Timestamp
+    var timeEnd: Timestamp
+    var joined: Int
+    var capacity: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case title
+        case location
+        case timeStart
+        case timeEnd
+        case joined
+        case capacity
+    }
+
+    static func ==(lhs: Meet, rhs: Meet) -> Bool {
+        return lhs.title == rhs.title
+    }
+
+    static func <(lhs: Meet, rhs: Meet) -> Bool {
+      return lhs.title < rhs.title
+    }
+    
+    func getStartString() -> String {
+        let timeStartDate = timeStart.dateValue()
+        print(timeStartDate)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM-dd-yyyy HH:mm"
+        return dateFormatter.string(from: timeStartDate)
+    }
+    
+    func getEndString() -> String {
+        let timeEndDate = timeEnd.dateValue()
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM-dd-yyyy HH:mm"
+        return dateFormatter.string(from: timeEndDate)
+    }
 }
+
