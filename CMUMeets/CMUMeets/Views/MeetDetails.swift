d9837825ca31050cc569a5f549aede4c7a9378f9
Isaac Ahn <75589282+isaacahn01@users.noreply.github.com>
2022-11-15 13:42:06 -0500

    Meet Details is Correct Now

--- CMUMeets/CMUMeets/Views/MeetDetails.swift
+++ CMUMeets/CMUMeets/Views/MeetDetails.swift
@@ -1,47 +1,23 @@
-import Foundation
 import SwiftUI
-import FirebaseFirestore
 
-struct MeetDetailsView: View {
-    @State var meet: Meet
-    @State private var clicked: Bool = false
-    @State private var presentAlert = false
-    @State private var meetsRepo = MeetsRepository()
-    
+struct MeetDetails: View {
+    @ObservedObject var meetsLibraryViewModel = MeetsLibraryViewModel()
+      
     var body: some View {
-        VStack {
-            Text(meet.title)
-            Text("Location: " + meet.location)
-            Text("Start: " + meet.getStartString())
-            Text("End: " + meet.getEndString())
-            Text("Joined: " + String(meet.joined))
-            Text("Capacity: " + String(meet.capacity))
-            Button("JOIN MEET") {
-                if meet.joined < meet.capacity {
-                    joinMeet()
-                    clicked = true
+        NavigationView {
+            List{
+                let meetViewModels = meetsLibraryViewModel.meetViewModels.sorted(by: { $0.meet < $1.meet })
+                ForEach(meetViewModels) { meetViewModel in
+                    MeetRowView(meet: meetViewModel.meet)
                 }
-                else {
-                    presentAlert = true
-                }
-            }
-            .alert("This Meet is full!", isPresented: $presentAlert, actions:{})
-            .disabled(clicked)
+            }.navigationBarTitle("Meets")
         }
     }
     
-    func joinMeet() {
-        let db = Firestore.firestore()
-        let path = db.collection("meets").document(self.meet.id!)
-        path.updateData([
-            "joined": meet.joined + 1
-        ]) { err in
-            if let err = err {
-                print("Error updating document: \(err)")
-            }
-            else {
-                print("Document updated successfully")
-            }
-        }
+}
+
+struct MeetDetails_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetDetails()
     }
 }
