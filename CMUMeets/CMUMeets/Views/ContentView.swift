0115b12e148d0238ab4b2821b27add476c02f05c
rickylee99 <rickyl@andrew.cmu.edu>
2022-11-16 18:02:59 -0500

    please please please work

--- CMUMeets/CMUMeets/Views/ContentView.swift
+++ CMUMeets/CMUMeets/Views/ContentView.swift
@@ -8,48 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
-    @ObservedObject var viewController: ViewController = ViewController()
-    @State private var showingHostView = false
-    var hostViewModel = HostViewModel()
-
-    var body: some View {
-        NavigationView {
-            VStack {
-              TabView {
-                VStack {
-                  MapView(viewController: viewController)
-                  Button("Host a meet") {
-                    showingHostView = true;
-                  }
-                  .sheet(isPresented: $showingHostView) {
-                    HostView(hostViewModel: hostViewModel)
-                  }
-                }
-                .tabItem {
-                  Image(systemName: "books.vertical")
-                  Text("MapView")
-                }
-                LocationView(viewController: viewController)
-                .tabItem {
-                  Image(systemName: "books.vertical")
-                  Text("LocationView")
-                }
-                DummyView(viewController: viewController)
-                .tabItem {
-                  Image(systemName: "books.vertical")
-                  Text("LocationView")
-                }
-                MeetDetails().tabItem {
-                  Image(systemName: "calendar")
-                }
-                NewUserView().tabItem {
-                  Image(systemName: "person.badge.plus")
-                }
-                CurrentUser().tabItem {
-                    Image(systemName: "person.crop.circle")
-                }
-              }
+  @ObservedObject var viewController: ViewController = ViewController()
+  @State private var showingHostView = false
+  var hostViewModel = HostViewModel()
+  
+  var body: some View {
+    NavigationView {
+      VStack {
+        TabView {
+          VStack {
+            MapView(viewController: viewController)
+            Button("Host a meet") {
+              showingHostView = true;
+            }
+            .sheet(isPresented: $showingHostView) {
+              HostView(hostViewModel: hostViewModel)
+            }
+          }
+          .tabItem {
+            Image(systemName: "books.vertical")
+            Text("MapView")
+          }
+          LocationView(viewController: viewController)
+            .tabItem {
+              Image(systemName: "books.vertical")
+              Text("LocationView")
+            }
+          DummyView(viewController: viewController)
+            .tabItem {
+              Image(systemName: "books.vertical")
+              Text("LocationView")
+            }
+          MeetDetails().tabItem {
+            Image(systemName: "calendar")
+          }
+          NewUserView().tabItem {
+            Image(systemName: "person.badge.plus")
+          }
+//          CurrentUser().tabItem {
+//            Image(systemName: "person.crop.circle")
+//          }
+        }
+      }
     }
+  }
 }
 
 
